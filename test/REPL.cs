428736c5e4bacfdad5c426e0e1dfe7e22b50c1bc
wangyh <wangyhzzzz@gmail.com>
2020-07-01 19:21:19 +0800

    test file

--- test/REPL.cs
+++ test/REPL.cs
@@ -21,14 +21,48 @@ public class REPL : MonoBehaviour
         var output = new StreamWriter(Console.OpenStandardOutput());
         output.AutoFlush = true;
         Console.SetOut(output);
+        Application.logMessageReceived += OnLogMessageReceived;
     }
+    private void LogConsole(string message, LogType logType)
+    {
+        if (string.IsNullOrEmpty(message)) return;
+        switch (logType)
+        {
+            case LogType.Error:
+            case LogType.Assert:
+            case LogType.Exception:
+                Console.ForegroundColor = ConsoleColor.Red;
+                break;
+            case LogType.Warning:
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                break;
+            default:
+                Console.ForegroundColor = ConsoleColor.White;
+                break;
+        }
+
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+
+    private void OnLogMessageReceived(string logMessage, string stackTrace, LogType logType)
+    {
+        LogConsole(logMessage, logType);
+        if (logType == LogType.Error)
+            LogConsole(stackTrace, logType);
+    }
+
     public void Update()
     {
         var str = Repl.getline();
         if (!string.IsNullOrEmpty(str))
         {
-            Debug.Log(str);
+            //Debug.Log(str);
             // luaState.DoString(str);
         }
+    }
+    protected virtual void OnDestroy()
+    {
+        Application.logMessageReceived -= OnLogMessageReceived;
     }
 }
